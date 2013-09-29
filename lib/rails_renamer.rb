bdd0f4e4747e0ba4ae267be320ceb8e734109efd
Joe Letizia <joe.letizia@gmail.com>
2013-09-29 11:24:10 -0400

    renames file_repository

--- lib/rails_renamer.rb
+++ lib/rails_renamer.rb
@@ -1,6 +1,6 @@
 require "rails_renamer/version"
 require 'rails_renamer/original_name_locator'
 require "rails_renamer/application_file_parser"
-require 'rails_renamer/files_to_rename'
+require 'rails_renamer/file_repository'
 require 'pry'
 
